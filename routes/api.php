e71c9adbccfccbf59ca5bd0d2a4f9b301a0abd7f
SeadSilajdzic <sead17@outlook.com>
2022-07-08 19:29:17 +0200

    PROJECT[init]: file system setup

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\Category\CategoryController;
+use App\Http\Controllers\Api\User\UserController;
+use App\Http\Controllers\CarController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('/user', UserController::class);
+Route::resource('/car', CarController::class);
+Route::resource('/category', CategoryController::class);
+
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
